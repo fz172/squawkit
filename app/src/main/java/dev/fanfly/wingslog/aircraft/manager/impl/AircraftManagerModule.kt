4788c5e5664428cdcf63459098efd25149a4357a
fanzhang172 <fanzhang172@gmail.com>
2026-01-29 13:18:56 -0800

    Basic CRUD for log manager

--- app/src/main/java/dev/fanfly/wingslog/aircraft/manager/impl/AircraftManagerModule.kt
+++ app/src/main/java/dev/fanfly/wingslog/aircraft/manager/impl/AircraftManagerModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.fanfly.wingslog.aircraft.manager.AircraftManager
+import dev.fanfly.wingslog.aircraft.manager.MaintenanceLogManager
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -13,4 +14,6 @@ interface AircraftManagerModule {
   @Binds
   fun bindAircraftManager(impl: AircraftManagerImpl): AircraftManager
 
+  @Binds
+  fun bindMaintenanceLogManager(impl: MaintenanceLogManagerImpl): MaintenanceLogManager
 }
