3b33450a71893d17d8863933b8bd11c832cca974
fz172 <fanzhang172@gmail.com>
2026-04-03 13:27:00 -0700

    fix: add InspectionViewModel to Koin module and fix resource imports in routes

--- feature/aircraft/inspection/src/commonMain/kotlin/dev/fanfly/wingslog/feature/aircraft/inspection/database/InspectionModule.kt
+++ feature/aircraft/inspection/src/commonMain/kotlin/dev/fanfly/wingslog/feature/aircraft/inspection/database/InspectionModule.kt
@@ -1,8 +1,11 @@
 package dev.fanfly.wingslog.feature.aircraft.inspection.database
 
 import dev.fanfly.wingslog.feature.aircraft.inspection.database.impl.InspectionManagerImpl
+import dev.fanfly.wingslog.feature.aircraft.inspection.ui.InspectionViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val inspectionModule = module {
   single<InspectionManager> { InspectionManagerImpl(get(), get()) }
+  viewModel { InspectionViewModel(get(), get(), get()) }
 }
