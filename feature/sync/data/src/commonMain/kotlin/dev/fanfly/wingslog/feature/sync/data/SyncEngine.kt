736fafad34c646abc6eca3998c90d529d44ce057
Fan Zhang <fanzhang172@gmail.com>
2026-07-10 09:19:42 -0700

    Apply formatter (import order + line wrap) to sync files

--- feature/sync/data/src/commonMain/kotlin/dev/fanfly/wingslog/feature/sync/data/SyncEngine.kt
+++ feature/sync/data/src/commonMain/kotlin/dev/fanfly/wingslog/feature/sync/data/SyncEngine.kt
@@ -9,8 +9,8 @@ import dev.fanfly.wingslog.core.storage.EntityScope
 import dev.fanfly.wingslog.core.storage.EntityStore
 import dev.fanfly.wingslog.core.storage.EntityStoreFactory
 import dev.fanfly.wingslog.core.storage.blob.BlobId
-import dev.fanfly.wingslog.core.storage.db.WingsLogDatabase
 import dev.fanfly.wingslog.core.storage.blob.UploadScheduler
+import dev.fanfly.wingslog.core.storage.db.WingsLogDatabase
 import dev.fanfly.wingslog.feature.sync.data.SyncEngine.Companion.PUSH_FAILURE_KEY
 import dev.gitlive.firebase.auth.FirebaseAuth
 import dev.gitlive.firebase.auth.FirebaseUser
