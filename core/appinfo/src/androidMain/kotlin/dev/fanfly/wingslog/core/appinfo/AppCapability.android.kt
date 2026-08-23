0e5323c9d7b6718ec35d465d1f34da6fe46c45c4
Fan Zhang <fanzhang172@gmail.com>
2026-08-23 15:41:05 -0700

    Update version number for notification N2 release candidate

--- core/appinfo/src/androidMain/kotlin/dev/fanfly/wingslog/core/appinfo/AppCapability.android.kt
+++ core/appinfo/src/androidMain/kotlin/dev/fanfly/wingslog/core/appinfo/AppCapability.android.kt
@@ -3,7 +3,7 @@ package dev.fanfly.wingslog.core.appinfo
 actual fun createAppCapability(isDeveloperBuild: Boolean) = AppCapability(
   isDeveloperOptionsSupported = isDeveloperBuild,
   isStressTestSupported = isDeveloperBuild,
-  isNotificationsSupported = isDeveloperBuild,
+  isNotificationsSupported = true,
   isCameraCaptureSupported = true,
   isAnonymousLoginSupported = true,
   // GA (#386, P9): was dev + dogfood only until ads GA. Off = NO ads.
