04d804c6d27a521cf065866ee42bb331ebc56966
fanzhang172 <fz172@users.noreply.github.com>
2026-09-16 22:44:08 -0700

    Fix attachment form compile error; rename device group to Files (#1062)
    * Restore the PickedDataLog import in AttachmentFormSection
    
    The reformat pass in c1baef75a dropped it as unused, but onAttachDataLogs still takes a
    List<PickedDataLog>, so feature:attachment:viewing stopped compiling.
    
    * Rename the attachment picker device group to Files

--- feature/attachment/viewing/src/commonMain/kotlin/dev/fanfly/wingslog/feature/attachment/viewing/AttachmentFormSection.kt
+++ feature/attachment/viewing/src/commonMain/kotlin/dev/fanfly/wingslog/feature/attachment/viewing/AttachmentFormSection.kt
@@ -35,6 +35,7 @@ import dev.fanfly.wingslog.core.ui.common.compose.AlertDialog
 import dev.fanfly.wingslog.core.ui.common.compose.FormSectionLabel
 import dev.fanfly.wingslog.core.ui.theme.Spacing
 import dev.fanfly.wingslog.feature.attachment.model.PendingAttachment
+import dev.fanfly.wingslog.feature.attachment.model.PickedDataLog
 import dev.fanfly.wingslog.feature.attachment.model.PickedFile
 import dev.fanfly.wingslog.feature.attachment.model.fileCount
 import dev.fanfly.wingslog.feature.attachment.model.isFile
